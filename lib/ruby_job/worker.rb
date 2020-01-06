mimperatore/async_job

initial Worker implementation

lib/async_job/worker.rb
@@ -1,0 +1,45 @@
+# frozen_string_literal: true
+
+module AsyncJob
+  module Worker
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def retry?(*)
+      false
+    end
+
+    private
+
+    def do_perform(*args)
+      @attempt ||= 1
+      perform(*args)
+    rescue StandardError => e
+      raise unless retry?(attempt: @attempt, error: e)
+
+      @attempt += 1
+      retry
+    end
+
+    module ClassMethods
+      def perform(*args)
+        worker = new
+        worker.send(:do_perform, *args)
+      end
+
+      def perform_async(*args)
+        AsyncJob.new(class_name: name, args: args)
+      end
+
+      def perform_at(at, *args)
+        AsyncJob.new(class_name: name, args: args, start_at: at)
+      end
+
+      def perform_in(in_ms, *args)
+        at = Time.now + Rational(in_ms, 1000)
+        AsyncJob.new(class_name: name, args: args, start_at: at)
+      end
+    end
+  end
+end

spec/async_job/async_job_spec.rb
@@ -1,0 +1,85 @@
+# frozen_string_literal: true
+
+module AsyncJob
+  RSpec.describe AsyncJob do
+    before(:each) do
+      Timecop.freeze(Time.now)
+    end
+
+    let(:class_name) { 'c' }
+    let(:args) { [1] }
+    let(:start_at) { Time.now }
+    let(:expected_hash) do
+      {
+        'json_class' => described_class.name,
+        'data' => {
+          'class_name' => class_name,
+          'args_json' => JSON.dump(args),
+          'start_at' => start_at.iso8601(9)
+        }
+      }
+    end
+
+    subject { described_class.new(class_name: class_name, args: args, start_at: start_at) }
+
+    describe '#==' do
+      context 'equality' do
+        it 'compares equal objects correctly' do
+          other = described_class.new(class_name: class_name, args: args, start_at: start_at)
+          expect(subject).to eq(other)
+        end
+
+        it 'compares equal objects correctly when args are nil' do
+          obj1 = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          obj2 = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          expect(obj1).to eq(obj2)
+        end
+      end
+
+      context 'inequality' do
+        it 'compares non-equal objects correctly (different class_name)' do
+          other = described_class.new(class_name: 'd', args: args, start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (different args)' do
+          other = described_class.new(class_name: class_name, args: [2], start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (one nil arg)' do
+          other = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (different start_at)' do
+          other = described_class.new(class_name: class_name, args: args, start_at: start_at + 1)
+          expect(subject).not_to eq(other)
+        end
+      end
+    end
+
+    describe '#to_h' do
+      it 'returns a hash representation of the object' do
+        expect(subject.to_h).to eq(expected_hash)
+      end
+    end
+
+    describe '#to_json' do
+      it 'returns the json encoding of #to_h, with the specified options' do
+        options = double('options')
+        hash = double('hash')
+        json = double('json')
+        expect(subject).to receive(:to_h).and_return(hash)
+        expect(hash).to receive(:to_json).with(options).and_return(json)
+        expect(subject.to_json(options)).to eq(json)
+      end
+    end
+
+    describe '.json_create' do
+      it 'returns an AsyncJob from the specified hash' do
+        expect(AsyncJob.json_create(JSON.parse(subject.to_json))).to eq(subject)
+      end
+    end
+  end
+end

spec/async_job/worker_spec.rb
@@ -1,0 +1,137 @@
+# frozen_string_literal: true
+
+module AsyncJob
+  RSpec.describe Worker do
+    let(:worker_class) do
+      Class.new do
+        include Worker
+
+        def perform(_, _, _); end
+      end
+    end
+
+    let(:a_worker) do
+      a_worker = worker_class.new
+      allow(worker_class).to receive(:new).and_return(a_worker)
+      a_worker
+    end
+
+    around(:each) do |example|
+      Object.const_set('MyWorker', worker_class)
+      example.run
+      Object.send(:remove_const, 'MyWorker')
+    end
+
+    describe '.perform' do
+      context 'method definition expectations' do
+        let(:worker_class) do
+          Class.new do
+            include Worker
+          end
+        end
+
+        it 'expects instances to define #perform' do
+          expect { MyWorker.perform(1, 2, 3) }.to raise_error(
+            NoMethodError, /undefined method `perform'/
+          )
+        end
+      end
+
+      context 'synchronous execution' do
+        it 'returns the value returned by #perform' do
+          allow(a_worker).to receive(:perform).once.with(1, 2, 3).and_return(77)
+          expect(MyWorker.perform(1, 2, 3)).to eq(77)
+        end
+
+        it 'immediately calls #perform on a newly-created instance of the worker class, with the supplied args' do
+          expect(a_worker).to receive(:perform).once.with(1, 2, 3)
+          MyWorker.perform(1, 2, 3)
+        end
+
+        context 'on failure' do
+          let(:runtime_error) { RuntimeError.new('boom') }
+          let(:worker_class) do
+            Class.new do
+              include Worker
+
+              def perform(_, _, _); end
+            end
+          end
+
+          let(:a_worker_that_fails) do
+            a_worker = worker_class.new
+            allow(worker_class).to receive(:new).and_return(a_worker)
+            a_worker
+          end
+
+          before(:each) do
+            allow(a_worker_that_fails).to receive(:perform).and_raise(runtime_error)
+          end
+
+          it 'does not retry by default' do
+            expect(a_worker_that_fails).to receive(:perform).once
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error(runtime_error)
+          end
+
+          it 'keeps calling #retry? on each failure, passing the # of the '\
+            'last attempt made, until #retry? returns false' do
+            4.times do |i|
+              expect(a_worker_that_fails).to receive(:retry?)
+                .with(hash_including(attempt: i + 1)).ordered.and_return(i < 3)
+            end
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+
+          it 'retries calling #perform each time #retry? returns true' do
+            4.times do |i|
+              expect(a_worker_that_fails).to receive(:perform).with(1, 2, 3).ordered
+              expect(a_worker_that_fails).to receive(:retry?)
+                .with(hash_including(attempt: i + 1)).ordered.and_return(i < 3)
+            end
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+
+          it 'passes the error riased to #retry?' do
+            expect(a_worker_that_fails).to receive(:retry?).with(hash_including(error: runtime_error)).and_return(false)
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error(runtime_error)
+          end
+
+          it 're-raises when #retry? returns false' do
+            allow(a_worker_that_fails).to receive(:retry?).and_return(false)
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+        end
+      end
+
+      context 'asynchronous execution' do
+        before(:each) do
+          Timecop.freeze(Time.now)
+        end
+
+        describe '.perform_async' do
+          it 'returns an AsyncJob for a worker with the specified arguments, that will start now' do
+            async_job = AsyncJob.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: Time.now)
+            expect(MyWorker.perform_async(1, 2, 3)).to eq(async_job)
+          end
+        end
+
+        describe '.perform_at' do
+          it 'returns an AsyncJob for a worker with the specified arguments, that will start at the specified time' do
+            later = Time.now + Rational(3.5, 1000)
+            async_job = AsyncJob.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: later)
+            expect(MyWorker.perform_at(later, 1, 2, 3)).to eq(async_job)
+          end
+        end
+
+        describe '.perform_in' do
+          it 'returns an AsyncJob for a worker with the specified arguments, '\
+            'that will start after the specified amount of time' do
+            later = Time.now + Rational(3.5, 1000)
+            async_job = AsyncJob.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: later)
+            expect(MyWorker.perform_in(3.5, 1, 2, 3)).to eq(async_job)
+          end
+        end
+      end
+    end
+  end
+end

mimperatore/ruby_job

lib/ruby_job/worker.rb
@@ -1,0 +1,45 @@
+# frozen_string_literal: true
+
+module RubyJob
+  module Worker
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def retry?(*)
+      false
+    end
+
+    private
+
+    def do_perform(*args)
+      @attempt ||= 1
+      perform(*args)
+    rescue StandardError => e
+      raise unless retry?(attempt: @attempt, error: e)
+
+      @attempt += 1
+      retry
+    end
+
+    module ClassMethods
+      def perform(*args)
+        worker = new
+        worker.send(:do_perform, *args)
+      end
+
+      def perform_async(*args)
+        Job.new(class_name: name, args: args)
+      end
+
+      def perform_at(at, *args)
+        Job.new(class_name: name, args: args, start_at: at)
+      end
+
+      def perform_in(in_ms, *args)
+        at = Time.now + Rational(in_ms, 1000)
+        Job.new(class_name: name, args: args, start_at: at)
+      end
+    end
+  end
+end

spec/ruby_job/async_job_spec.rb
@@ -1,0 +1,85 @@
+# frozen_string_literal: true
+
+module RubyJob
+  RSpec.describe Job do
+    before(:each) do
+      Timecop.freeze(Time.now)
+    end
+
+    let(:class_name) { 'c' }
+    let(:args) { [1] }
+    let(:start_at) { Time.now }
+    let(:expected_hash) do
+      {
+        'json_class' => described_class.name,
+        'data' => {
+          'class_name' => class_name,
+          'args_json' => JSON.dump(args),
+          'start_at' => start_at.iso8601(9)
+        }
+      }
+    end
+
+    subject { described_class.new(class_name: class_name, args: args, start_at: start_at) }
+
+    describe '#==' do
+      context 'equality' do
+        it 'compares equal objects correctly' do
+          other = described_class.new(class_name: class_name, args: args, start_at: start_at)
+          expect(subject).to eq(other)
+        end
+
+        it 'compares equal objects correctly when args are nil' do
+          obj1 = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          obj2 = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          expect(obj1).to eq(obj2)
+        end
+      end
+
+      context 'inequality' do
+        it 'compares non-equal objects correctly (different class_name)' do
+          other = described_class.new(class_name: 'd', args: args, start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (different args)' do
+          other = described_class.new(class_name: class_name, args: [2], start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (one nil arg)' do
+          other = described_class.new(class_name: class_name, args: nil, start_at: start_at)
+          expect(subject).not_to eq(other)
+        end
+
+        it 'compares non-equal objects correctly (different start_at)' do
+          other = described_class.new(class_name: class_name, args: args, start_at: start_at + 1)
+          expect(subject).not_to eq(other)
+        end
+      end
+    end
+
+    describe '#to_h' do
+      it 'returns a hash representation of the object' do
+        expect(subject.to_h).to eq(expected_hash)
+      end
+    end
+
+    describe '#to_json' do
+      it 'returns the json encoding of #to_h, with the specified options' do
+        options = double('options')
+        hash = double('hash')
+        json = double('json')
+        expect(subject).to receive(:to_h).and_return(hash)
+        expect(hash).to receive(:to_json).with(options).and_return(json)
+        expect(subject.to_json(options)).to eq(json)
+      end
+    end
+
+    describe '.json_create' do
+      it 'returns a Job from the specified hash' do
+        expect(Job.json_create(JSON.parse(subject.to_json))).to eq(subject)
+      end
+    end
+  end
+end

spec/ruby_job/worker_spec.rb
@@ -1,0 +1,137 @@
+# frozen_string_literal: true
+
+module RubyJob
+  RSpec.describe Worker do
+    let(:worker_class) do
+      Class.new do
+        include Worker
+
+        def perform(_, _, _); end
+      end
+    end
+
+    let(:a_worker) do
+      a_worker = worker_class.new
+      allow(worker_class).to receive(:new).and_return(a_worker)
+      a_worker
+    end
+
+    around(:each) do |example|
+      Object.const_set('MyWorker', worker_class)
+      example.run
+      Object.send(:remove_const, 'MyWorker')
+    end
+
+    describe '.perform' do
+      context 'method definition expectations' do
+        let(:worker_class) do
+          Class.new do
+            include Worker
+          end
+        end
+
+        it 'expects instances to define #perform' do
+          expect { MyWorker.perform(1, 2, 3) }.to raise_error(
+            NoMethodError, /undefined method `perform'/
+          )
+        end
+      end
+
+      context 'synchronous execution' do
+        it 'returns the value returned by #perform' do
+          allow(a_worker).to receive(:perform).once.with(1, 2, 3).and_return(77)
+          expect(MyWorker.perform(1, 2, 3)).to eq(77)
+        end
+
+        it 'immediately calls #perform on a newly-created instance of the worker class, with the supplied args' do
+          expect(a_worker).to receive(:perform).once.with(1, 2, 3)
+          MyWorker.perform(1, 2, 3)
+        end
+
+        context 'on failure' do
+          let(:runtime_error) { RuntimeError.new('boom') }
+          let(:worker_class) do
+            Class.new do
+              include Worker
+
+              def perform(_, _, _); end
+            end
+          end
+
+          let(:a_worker_that_fails) do
+            a_worker = worker_class.new
+            allow(worker_class).to receive(:new).and_return(a_worker)
+            a_worker
+          end
+
+          before(:each) do
+            allow(a_worker_that_fails).to receive(:perform).and_raise(runtime_error)
+          end
+
+          it 'does not retry by default' do
+            expect(a_worker_that_fails).to receive(:perform).once
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error(runtime_error)
+          end
+
+          it 'keeps calling #retry? on each failure, passing the # of the '\
+            'last attempt made, until #retry? returns false' do
+            4.times do |i|
+              expect(a_worker_that_fails).to receive(:retry?)
+                .with(hash_including(attempt: i + 1)).ordered.and_return(i < 3)
+            end
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+
+          it 'retries calling #perform each time #retry? returns true' do
+            4.times do |i|
+              expect(a_worker_that_fails).to receive(:perform).with(1, 2, 3).ordered
+              expect(a_worker_that_fails).to receive(:retry?)
+                .with(hash_including(attempt: i + 1)).ordered.and_return(i < 3)
+            end
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+
+          it 'passes the error riased to #retry?' do
+            expect(a_worker_that_fails).to receive(:retry?).with(hash_including(error: runtime_error)).and_return(false)
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error(runtime_error)
+          end
+
+          it 're-raises when #retry? returns false' do
+            allow(a_worker_that_fails).to receive(:retry?).and_return(false)
+            expect { MyWorker.perform(1, 2, 3) }.to raise_error('boom')
+          end
+        end
+      end
+
+      context 'asynchronous execution' do
+        before(:each) do
+          Timecop.freeze(Time.now)
+        end
+
+        describe '.perform_async' do
+          it 'returns an Job for a worker with the specified arguments, that will start now' do
+            job = Job.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: Time.now)
+            expect(MyWorker.perform_async(1, 2, 3)).to eq(job)
+          end
+        end
+
+        describe '.perform_at' do
+          it 'returns a Job for a worker with the specified arguments, that will start at the specified time' do
+            later = Time.now + Rational(3.5, 1000)
+            job = Job.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: later)
+            expect(MyWorker.perform_at(later, 1, 2, 3)).to eq(job)
+          end
+        end
+
+        describe '.perform_in' do
+          it 'returns a Job for a worker with the specified arguments, '\
+            'that will start after the specified amount of time' do
+            later = Time.now + Rational(3.5, 1000)
+            job = Job.new(class_name: 'MyWorker', args: [1, 2, 3], start_at: later)
+            expect(MyWorker.perform_in(3.5, 1, 2, 3)).to eq(job)
+          end
+        end
+      end
+    end
+  end
+end