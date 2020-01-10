mimperatore/async_job

initial JobProcessor implementation

lib/async_job/job_processor.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+module AsyncJob
+  class JobProcessor
+    def initialize(jobstore)
+      @jobstore = jobstore
+    end
+
+    def run(wait: false)
+      loop do
+        async_job = @jobstore.fetch(wait: wait)
+        async_job ? async_job.perform : break
+      end
+    end
+  end
+end

spec/async_job/job_processor_spec.rb
@@ -1,0 +1,50 @@
+# frozen_string_literal: true
+
+module AsyncJob
+  RSpec.describe JobProcessor do
+    let(:worker_class) do
+      Class.new do
+        include Worker
+
+        def perform(_, _, _); end
+
+        def self.jobstore; end
+      end
+    end
+
+    around(:each) do |example|
+      Object.const_set('MyWorker', worker_class)
+      example.run
+      Object.send(:remove_const, 'MyWorker')
+    end
+
+    describe '#run' do
+      let(:jobstore) { JobStore.new }
+
+      subject do
+        described_class.new(jobstore)
+      end
+
+      context 'order of execution' do
+        before(:each) do
+          job1 = AsyncJob.new(worker_class_name: 'MyWorker', args: nil)
+          job2 = AsyncJob.new(worker_class_name: 'MyWorker', args: nil)
+          allow(jobstore).to receive(:fetch).and_return(job1, job2, nil)
+          allow(job1).to receive(:perform).ordered
+          allow(job2).to receive(:perform).ordered
+        end
+
+        it "processes the jobs in the order returned by the jobstore's #fetch order" do
+          subject.run
+        end
+      end
+
+      context 'waiting for jobs' do
+        it "uses the jobstore's #fetch(wait: true) method" do
+          expect(jobstore).to receive(:fetch).with(wait: true)
+          subject.run(wait: true)
+        end
+      end
+    end
+  end
+end

mimperatore/ruby_job

lib/ruby_job/job_processor.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+module RubyJob
+  class JobProcessor
+    def initialize(jobstore)
+      @jobstore = jobstore
+    end
+
+    def run(wait: false)
+      loop do
+        job = @jobstore.fetch(wait: wait)
+        job ? job.perform : break
+      end
+    end
+  end
+end

spec/ruby_job/job_processor_spec.rb
@@ -1,0 +1,50 @@
+# frozen_string_literal: true
+
+module RubyJob
+  RSpec.describe JobProcessor do
+    let(:worker_class) do
+      Class.new do
+        include Worker
+
+        def perform(_, _, _); end
+
+        def self.jobstore; end
+      end
+    end
+
+    around(:each) do |example|
+      Object.const_set('MyWorker', worker_class)
+      example.run
+      Object.send(:remove_const, 'MyWorker')
+    end
+
+    describe '#run' do
+      let(:jobstore) { JobStore.new }
+
+      subject do
+        described_class.new(jobstore)
+      end
+
+      context 'order of execution' do
+        before(:each) do
+          job1 = Job.new(worker_class_name: 'MyWorker', args: nil)
+          job2 = Job.new(worker_class_name: 'MyWorker', args: nil)
+          allow(jobstore).to receive(:fetch).and_return(job1, job2, nil)
+          allow(job1).to receive(:perform).ordered
+          allow(job2).to receive(:perform).ordered
+        end
+
+        it "processes the jobs in the order returned by the jobstore's #fetch order" do
+          subject.run
+        end
+      end
+
+      context 'waiting for jobs' do
+        it "uses the jobstore's #fetch(wait: true) method" do
+          expect(jobstore).to receive(:fetch).with(wait: true)
+          subject.run(wait: true)
+        end
+      end
+    end
+  end
+end